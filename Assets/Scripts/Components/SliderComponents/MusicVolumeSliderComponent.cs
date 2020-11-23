9c2c45aad6e123410832af664ac1db200117230e
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2020-11-23 16:52:12 +0300

    Add setting of background music volume via slider in settings window

---
+++ Assets/Scripts/Components/SliderComponents/MusicVolumeSliderComponent.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+namespace Components.SliderComponents
+{
+    [RequireComponent(typeof(Slider))]
+    public class MusicVolumeSliderComponent : MonoBehaviour
+    {
+        [SerializeField]
+        private AudioMixer _audioMixer;
+
+        private void Awake()
+        {
+            _audioMixer.GetFloat("MusicVolume", out var value);
+            this.gameObject.GetComponent<Slider>().value = value;
+
+            this.gameObject.GetComponent<Slider>().onValueChanged.AddListener(OnValueChanged);
+        }
+        private void OnDestroy()
+        {
+            this.gameObject.GetComponent<Slider>().onValueChanged.RemoveListener(OnValueChanged);
+        }
+
+        private void OnValueChanged(float value)
+        {
+            _audioMixer.SetFloat("MusicVolume", value);
+        }
+    }
+}
