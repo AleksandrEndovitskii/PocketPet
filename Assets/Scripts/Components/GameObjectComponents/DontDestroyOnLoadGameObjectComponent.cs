e03c4367bc10105b7ff85edf1f3d12dea14233ba
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2020-11-23 16:30:12 +0300

    Add background music playing in all scenes

---
+++ Assets/Scripts/Components/GameObjectComponents/DontDestroyOnLoadGameObjectComponent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Components.GameObjectComponents
+{
+    public class DontDestroyOnLoadGameObjectComponent : MonoBehaviour
+    {
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject); // sets this to not be destroyed when reloading scene
+        }
+    }
+}
