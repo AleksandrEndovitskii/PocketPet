4c1c4e4da784f95028b796aa928db1331d142689
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2020-11-23 20:27:03 +0300

    Add character movement

---
+++ Assets/Scripts/Components/Character/CharacterControllerMovementComponent.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Components.Character
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class CharacterControllerMovementComponent : MonoBehaviour
+    {
+        [SerializeField]
+        private float _speed = 10f;
+
+        private CharacterController _characterController;
+
+        private void Awake()
+        {
+            _characterController = this.gameObject.GetComponent<CharacterController>();
+        }
+
+        private void Update()
+        {
+            var x = Input.GetAxis("Horizontal");
+            var z = Input.GetAxis("Vertical");
+
+            var move = this.gameObject.transform.right * x + this.gameObject.transform.forward * z;
+
+            _characterController.Move(move * _speed * Time.deltaTime);
+        }
+    }
+}
