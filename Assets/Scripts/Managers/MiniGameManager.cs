2099c3b22c7d381028b96c624e8386806b04c618
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2020-11-28 01:29:15 +0300

    Add instantiating of mini game via mini game manager

---
+++ Assets/Scripts/Managers/MiniGameManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Components.GameObjectComponents;
+using UnityEngine;
+using Utils;
+
+namespace Managers
+{
+    public class MiniGameManager : MonoBehaviour, IInitilizable
+    {
+        [SerializeField]
+        private ObjectsInstantiatingComponent _prefab;
+        [SerializeField]
+        private Transform _parent;
+        [NonSerialized]
+        public ObjectsInstantiatingComponent Instance;
+
+        private void Awake()
+        {
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            Instance = Instantiate(_prefab, _parent);
+        }
+    }
+}
