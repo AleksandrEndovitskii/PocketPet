f9b59557e1a6758b807ed287157ad76ace026af4
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2020-11-27 15:07:24 +0300

    Add input manager

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Managers
+{
+    public class InputManager : MonoBehaviour
+    {
+        public Action<KeyCode> KeyPressed = delegate {  };
+
+        [SerializeField]
+        private List<KeyCode> _trakedKeyCodes = new List<KeyCode>();
+
+        private void Update()
+        {
+            if (_trakedKeyCodes.Any(Input.GetKeyDown))
+            {
+                var pressedKeys = _trakedKeyCodes.Where(Input.GetKeyDown);
+
+                foreach (var pressedKey in pressedKeys)
+                {
+                    Debug.Log($"Key({pressedKey}) was pressed");
+
+                    KeyPressed.Invoke(pressedKey);
+                }
+            }
+        }
+    }
+}
