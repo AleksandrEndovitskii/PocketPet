8c84210fa16aca03236047c55a77cf6daccc56d0
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2020-11-27 20:32:08 +0300

    Add balls container

---
+++ Assets/Scripts/Components/GameObjectComponents/ObjectsInstantiatingComponent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Components.GameObjectComponents
+{
+    public class ObjectsInstantiatingComponent : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _prefab;
+        [SerializeField]
+        private int _instancesCount;
+        [SerializeField]
+        private Transform _parent;
+
+        private List<GameObject> _instances = new List<GameObject>();
+
+        private void Awake()
+        {
+            for (var i = 0; i < _instancesCount; i++)
+            {
+                var instance = Instantiate(_prefab, _parent);
+
+                _instances.Add(instance);
+            }
+        }
+    }
+}
