0ffeb70193ab9a2e1567a2294ada3020f55399f1
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2020-11-29 01:47:17 +0300

    Move animation playing from click event of button to interact event of interactable

--- Assets/Scripts/Components/InteractionComponents/ButtonClickInteractableComponent.cs
+++ Assets/Scripts/Components/InteractionComponents/ButtonClickInteractableComponent.cs
@@ -1,5 +1,6 @@
 ﻿using Managers;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace Components.InteractionComponents
@@ -7,16 +8,24 @@ namespace Components.InteractionComponents
     [RequireComponent(typeof(Button))]
     public class ButtonClickInteractableComponent : MonoBehaviour, IInteractable
     {
+        // Event delegates triggered on click.
+        [SerializeField]
+        private Button.ButtonClickedEvent OnInteract = new Button.ButtonClickedEvent();
+
         private Button _button;
 
         private void Awake()
         {
             _button = this.gameObject.GetComponent<Button>();
 
+            _button.onClick.AddListener(ButtonOnClick);
+
             GameManager.Instance.InteractionManager.TrackInteractable(this);
         }
         private void OnDestroy()
         {
+            _button.onClick.RemoveListener(ButtonOnClick);
+
             if (GameManager.Instance.InteractionManager != null)
             {
                 GameManager.Instance.InteractionManager.UnTrackInteractable(this);
@@ -25,9 +34,17 @@ namespace Components.InteractionComponents
 
         public void Interact()
         {
-            _button.onClick.Invoke();
+            // invoke all additional actions specified in inspector
+            OnInteract.Invoke();
 
             GameManager.Instance.InteractionManager.InvokeInteraction(this);
+        }
+
+        private void ButtonOnClick()
+        {
+            Debug.Log("ButtonClickInteractableComponent button is pressed");
+
+            Interact();
         }
     }
 }
