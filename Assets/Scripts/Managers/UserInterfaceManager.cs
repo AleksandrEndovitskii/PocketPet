af16f734832f8fdb6e5c4fa297317a56da0b2a25
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2020-11-29 16:12:27 +0300

    Add instantiating of user interface for main menu scene

--- Assets/Scripts/Managers/UserInterfaceManager.cs
+++ Assets/Scripts/Managers/UserInterfaceManager.cs
@@ -1,13 +1,48 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 using Utils;
 
 namespace Managers
 {
-    public class UserInterfaceManager : MonoBehaviour, IInitilizable
+    public class UserInterfaceManager : SerializedMonoBehaviour, IInitilizable
     {
+        [SerializeField]
+        private Canvas _canvasPrefab;
+        [SerializeField]
+        private EventSystem _eventSystemPrefab;
+        [SerializeField]
+        private Dictionary<string, RectTransform> _sceneNameUserInterfacePrefabs = new Dictionary<string, RectTransform>();
+
+        private Canvas _canvasInstance;
+        private EventSystem _eventSystemInstance;
+        private RectTransform _userInterfaceInstance;
+
         public void Initialize()
         {
+            SceneManager.activeSceneChanged += SceneManagerOnActiveSceneChanged;
+        }
 
+        private void SceneManagerOnActiveSceneChanged(Scene previousScene, Scene currentScene)
+        {
+            InstantiateUserInterfaceForCurrentScene(currentScene);
+        }
+
+        private void InstantiateUserInterfaceForCurrentScene(Scene currentScene)
+        {
+            var keyValuePair = _sceneNameUserInterfacePrefabs.FirstOrDefault(x =>
+                x.Key == currentScene.name);
+            if (keyValuePair.Key == null)
+            {
+                return;
+            }
+
+            _canvasInstance = Instantiate(_canvasPrefab);
+            _eventSystemInstance = Instantiate(_eventSystemPrefab);
+            _userInterfaceInstance = Instantiate(keyValuePair.Value, _canvasInstance.transform);
         }
     }
 }
